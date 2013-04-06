7aa2faf359ad6eb6b1a6baeaf0f8d0bac7896981
Brad Touesnard <brad@touesnard.com>
2013-04-06 14:01:23 -0300

    Prepping for Disqus comments.

--- public_html/content/themes/bradtca/single.php
+++ public_html/content/themes/bradtca/single.php
@@ -26,18 +26,6 @@
 				<h3>Related Posts</h3>
 				<?php wp23_related_posts(); ?>
 			</div>
-			
-			<script type="text/javascript"><!--
-			google_ad_client = "pub-0807380235473840";
-			/* 468x15, created 5/13/08 */
-			google_ad_slot = "4662006954";
-			google_ad_width = 468;
-			google_ad_height = 15;
-			//-->
-			</script>
-			<script type="text/javascript"
-			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-			</script>
 
 		</div>
 
