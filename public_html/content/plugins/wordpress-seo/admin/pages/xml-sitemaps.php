80d745c274bfff2875b3fab886d9482c582dcb7d
Brad Touesnard <brad@touesnard.com>
2013-06-24 10:26:52 -0400

    Update plugins.

--- public_html/content/plugins/wordpress-seo/admin/pages/xml-sitemaps.php
+++ public_html/content/plugins/wordpress-seo/admin/pages/xml-sitemaps.php
@@ -10,7 +10,7 @@ if ( !defined('WPSEO_VERSION') ) {
 
 global $wpseo_admin_pages;
 
-$wpseo_admin_pages->admin_header( 'XML Sitemaps', true, 'yoast_wpseo_xml_sitemap_options', 'wpseo_xml' );
+$wpseo_admin_pages->admin_header( __( 'XML Sitemaps', 'wordpress-seo' ), true, 'yoast_wpseo_xml_sitemap_options', 'wpseo_xml' );
 
 $options = get_option( 'wpseo_xml' );
 
@@ -30,20 +30,26 @@ if ( isset( $options[ 'enablexmlsitemap' ] ) && $options[ 'enablexmlsitemap' ] )
 	$content .= '<p>' . sprintf( __( 'You can find your XML Sitemap here: %sXML Sitemap%s', 'wordpress-seo' ), '<a target="_blank" class="button-secondary" href="' . home_url( $base . 'sitemap_index.xml' ) . '">', '</a>' ) . '<br/><br/>' . __( 'You do <strong>not</strong> need to generate the XML sitemap, nor will it take up time to generate after publishing a post.', 'wordpress-seo' ) . '</p>';
 else
 	$content .= '<p>' . __( 'Save your settings to activate XML Sitemaps.', 'wordpress-seo' ) . '</p>';
+$content .= $wpseo_admin_pages->checkbox( 'disable_author_sitemap', __( "Disable author/user sitemap", 'wordpress-seo' ), false );
+$content .= '<br/>';
 $content .= '<strong>' . __( 'General settings', 'wordpress-seo' ) . '</strong><br/>';
 $content .= '<p>' . __( 'After content publication, the plugin automatically pings Google and Bing, do you need it to ping other search engines too? If so, check the box:', 'wordpress-seo' ) . '</p>';
 $content .= $wpseo_admin_pages->checkbox( 'xml_ping_yahoo', __( "Ping Yahoo!", 'wordpress-seo' ), false );
 $content .= $wpseo_admin_pages->checkbox( 'xml_ping_ask', __( "Ping Ask.com", 'wordpress-seo' ), false );
 $content .= '<br/><strong>' . __( 'Exclude post types', 'wordpress-seo' ) . '</strong><br/>';
 $content .= '<p>' . __( 'Please check the appropriate box below if there\'s a post type that you do <strong>NOT</strong> want to include in your sitemap:', 'wordpress-seo' ) . '</p>';
-foreach ( get_post_types( array( 'public' => true ), 'objects' ) as $pt ) {
+
+$post_types = apply_filters( 'wpseo_sitemaps_supported_post_types', get_post_types( array( 'public' => true ), 'objects' ) );
+foreach ( $post_types as $pt ) {
 	$content .= $wpseo_admin_pages->checkbox( 'post_types-' . $pt->name . '-not_in_sitemap', $pt->labels->name . ' (<code>' . $pt->name . '</code>)' );
 }
 
 $content .= '<br/>';
 $content .= '<strong>' . __( 'Exclude taxonomies', 'wordpress-seo' ) . '</strong><br/>';
 $content .= '<p>' . __( 'Please check the appropriate box below if there\'s a taxonomy that you do <strong>NOT</strong> want to include in your sitemap:', 'wordpress-seo' ) . '</p>';
-foreach ( get_taxonomies( array( 'public' => true ), 'objects' ) as $tax ) {
+
+$taxonomies = apply_filters( 'wpseo_sitemaps_supported_taxonomies', get_taxonomies( array( 'public' => true ), 'objects' ) );
+foreach ( $taxonomies as $tax ) {
 	if ( isset( $tax->labels->name ) && trim( $tax->labels->name ) != '' )
 		$content .= $wpseo_admin_pages->checkbox( 'taxonomies-' . $tax->name . '-not_in_sitemap', $tax->labels->name . ' (<code>' . $tax->name . '</code>)' );
 }
