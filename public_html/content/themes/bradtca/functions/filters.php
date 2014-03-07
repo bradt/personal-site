f2b6e78acef245db7be528135b101a7394f74758
Brad Touesnard <brad@touesnard.com>
2014-03-07 12:25:31 -0400

    Bug: Responsive images don't work in Gmail.

--- public_html/content/themes/bradtca/functions/filters.php
+++ public_html/content/themes/bradtca/functions/filters.php
@@ -38,3 +38,32 @@ function bt_embed_oembed_html( $html ) {
 	return preg_replace( '@src="https?:@', 'class="oembed" src="', $html );
 }
 add_filter( 'embed_oembed_html', 'bt_embed_oembed_html' );
+
+// I'm using Campaing Monitor's RSS to Email to send out email newsletter.
+// Gmail strips out height:auto; from inline styles, so
+// we can't do responsive images, so we just resize all images in the feed
+function bt_the_content_feed( $content ) {
+	if ( !preg_match_all( '@<img (.*?)>@', $content, $imgs ) ) {
+		return $content;
+	}
+
+	$img_count = count( $imgs[0] );
+	for ( $i = 0; $i < $img_count; $i++ ) {
+		$img = $imgs[0][$i];
+
+		if ( !preg_match( '@width="(.*?)"@', $img, $width ) || !preg_match( '@height="(.*?)"@', $img, $height ) ) {
+			continue;
+		}
+
+		$new_width = 580;
+		if ( $width[1] > $new_width ) {
+			$new_height = floor( $new_width / $width[1] * $height[1] );
+			$new_img = str_replace( $width[0], 'width="' . $new_width . '"', $img );
+			$new_img = str_replace( $height[0], 'height="' . $new_height . '"', $new_img );
+			$content = str_replace( $img, $new_img, $content );
+		}
+	}
+
+	return $content;
+}
+add_filter( 'the_content_feed', 'bt_the_content_feed' );
