41ed94d4dcee91150bcbc30759e0274302699a61
Brad Touesnard <brad@touesnard.com>
2014-04-01 16:01:47 -0300

    Update about page.

--- public_html/content/themes/bradtca/page-about.php
+++ public_html/content/themes/bradtca/page-about.php
@@ -11,19 +11,19 @@ get_header();
 	<div class="entry-content">
 
 		<p>I was born an <strong>entrepreneur</strong>. From lemonade stands and mowing lawns, to
-		making my first dollars online, I've always been motivated by
-		entrepreneurship. In June 2002, I cofounded <a title="Zenutech"
-		href="http://www.zenutech.ca/">a web hosting company</a> 
-		that's still
-		going strong today. I currently work full time on a new venture
-		started in October 2011 called <a 
-		href="http://deliciousbrains.com">Delicious Brains</a>.</p>
+		making my first dollars online, I've always been motivated to solve
+		problems for people.  I currently work full time on 
+		<a style="white-space:nowrap;" href="http://deliciousbrains.com/?utm_source=bradt.ca&utm_medium=web&utm_content=deliciousbrainsinc&utm_campaign=bradtca-about-page">Delicious Brains Inc,</a>
+		a company I started in December 2012 to develop and sell
+		<a style="white-space:nowrap;" href="http://deliciousbrains.com/wp-migrate-db-pro/?utm_source=bradt.ca&utm_medium=web&utm_content=wpmdbpro&utm_campaign=bradtca-about-page">WP Migrate DB Pro</a>,
+		a database migration plugin for WordPress.</p>
 
 		<p>Before that, I built web sites for a living. Working as an
 		independent, I collaborated with creative agencies and
 		independent web designers. I did frontend and backend development, 
-		working with SilverStripe CMS, and WordPress.
-		<a href="http://www.linkedin.com/in/bradt">View my LinkedIn profile.</a></p>
+		working with SilverStripe CMS and WordPress. See
+		<a href="http://www.linkedin.com/in/bradt">my LinkedIn profile</a> for more details
+		on my past work.</p>
 
 		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/travel.jpg" alt="" /></p>
 
@@ -53,7 +53,135 @@ get_header();
 		href="http://twitter.com/bradt">Twitter</a> and 
 		<a href="https://plus.google.com/103798016457548612622">Google+</a>
 		and post code to <a href="https://github.com/bradt">GitHub</a> and 
-		<a href="http://profiles.wordpress.org/bradt">WordPress.org</a>.</p>
+		<a href="http://profiles.wordpress.org/bradt">WordPress.org</a>. 
+		I also cohost the
+		<a href="http://applyfilters.fm">Apply Filters</a> podcast.</p>
+
+		<h1 id="speaking">Speaking</h1>
+
+		<table>
+		<tr>
+			<td>May 9-11, 2014</td>
+			<td><a href="http://2014.miami.wordcamp.org/">WordCamp Miami</a></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Dec 7, 2013</td>
+			<td><a href="http://wordsesh.org/">WordSesh 2</a></td>
+			<td><a href="https://www.youtube.com/watch?v=ipkjTCJ1EFw">Video</a></td>
+		</tr>
+		<tr>
+			<td>Jun 24, 2013</td>
+			<td><a href="http://www.meetup.com/boston-wordpress-meetup/events/111165722/">BostonWP Meetup</a></td>
+			<td><a href="https://deliciousbrains.com/bostonwp-meetup-presentation-video/">Video &amp; Transcript</a></td>
+		</tr>
+		<tr>
+			<td>Jan 20, 2012</td>
+			<td><a href="http://podcamphalifax.ca/">Podcamp Halifax</a></td>
+			<td><a href="/blog/podcamp-halifax-2012-wordpress-workshop/">Summary &amp; Notes</a>
+		</tr>
+		<tr>
+			<td>Oct 27, 2011</td>
+			<td><a href="http://aimconference.com/">Atlantic Interactive Marketing Conference</a></td>
+			<td><a href="/blog/aim-conference-workshop-notes-getting-started-with-wordpress/">Summary &amp; Notes</a>
+		</tr>
+		<tr>
+			<td>Jan 23, 2011</td>
+			<td><a href="http://podcamphalifax.ca/">Podcamp Halifax</a></td>
+			<td><a href="/blog/podcamphalifax-2011-review/">Slides &amp; Review</a>
+		</tr>
+		<tr>
+			<td>Sep 27, 2008</td>
+			<td>WordCamp Vancouver</a></td>
+			<td><a href="/blog/speaking-at-barcampvancouver2008/">Summary &amp; Slides</a></td>
+		</tr>
+		</table>
+
+		<h1 id="conferences">Conference Attendance</h1>
+
+		<table>
+		<tr>
+			<td>Apr 14-15, 2014</td>
+			<td><a href="http://www.microconf.com/">MicroConf</a></td>
+		</tr>
+		<tr>
+			<td>Jan 19, 2014</td>
+			<td><a href="http://podcamphalifax.ca/">Podcamp Halifax</a></td>
+		</tr>
+		<tr>
+			<td>Oct 17-19, 2013</td>
+			<td><a href="http://pressnomics.com/">PressNomics</a> (Phoenix)</td>
+		</tr>
+		<tr>
+			<td>Nov 9-10, 2012</td>
+			<td><a href="http://pressnomics.com/">PressNomics</a> (Phoenix)</td>
+			<td><a href="/blog/pressnomics-conference-recap/">Recap</a>
+		</tr>
+		<tr>
+			<td>Feb 6-8, 2008</td>
+			<td><a href="http://www.webdirections.org/events/#north08">Web Directions North</a> (Vancouver)</td>
+			<td>Blog Posts:
+				<a href="/blog/attending-web-directions-north-2008/" title="Attending Web Directions North 2008">1</a>,
+				<a href="/blog/wdn08-kicks-off/" title="WDN08 Kicks Off">2</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Feb 6-8, 2007</td>
+			<td><a href="http://www.webdirections.org/events/#north07">Web Directions North</a> (Vancouver)</td>
+			<td>Blog Posts:
+				<a href="/blog/attending-web-directions-north/" title="Attending Web Directions North">1</a>,
+				<a href="/blog/web-directions-north-day-0/" title="Web Directions North Day 1">2</a>,
+				<a href="/blog/web-directions-north-day-1/" title="Web Directions North Day 2">3</a>,
+				<a href="/blog/web-directions-north-day-2/" title="Web Directions North Day 3">4</a>,
+				<a href="/blog/web-directions-north-wrapup/" title="Web Directions North Wrapup">5</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Jul 25, 2006</td>
+			<td><a href="http://www.oscon.com/">OSCON</a></td>
+			<td><a href="/blog/oreilly-open-source-convention/">Preview</a>, <a href="/documents/oscon-2006-report/">Notes</a></td>
+		</tr>
+		</table>
+
+		<h1 id="interviews">Interviews</h1>
+
+		<table>
+		<tr>
+			<td>Dec 5, 2013</td>
+			<td><a href="http://casjam.com/consulting-vs-products-brad-touesnard/" title="Consulting vs. Products (or both?) with Brad Touesnard">Bootstrapped Web</a></td>
+			<td>Audio</td>
+		</tr>
+		<tr>
+			<td>Nov 28, 2013</td>
+			<td><a href="http://www.wpelevation.com/2013/11/episode-11-brad-touesnard/" title="Episode 11: Brad Touesnard">WP Elevation</a></td>
+			<td>Video</td>
+		</tr>
+		<tr>
+			<td>Nov 19, 2013</td>
+			<td><a href="http://wpengine.com/2013/11/19/brad-touesnard-building-wordpress-product-business/" title="Brad Touesnard on Building a WordPress Product Business">WP Engine</a></td>
+			<td>Article</td>
+		</tr>
+		<tr>
+			<td>Sep 24, 2013</td>
+			<td><a href="http://torquemag.io/the-end-of-wp-app-store/" title="The End of WP App Store">Torque</a></td>
+			<td>Article</td>
+		</tr>
+		<tr>
+			<td>Aug 20, 2013</td>
+			<td><a href="http://mattreport.com/episode-46-the-benefits-of-failure/" title="Episode 46: The benefits of failure">Matt Report</a></td>
+			<td>Audio</td>
+		</tr>
+		<tr>
+			<td>Jul 4, 2012</td>
+			<td><a href="http://yourwebsiteengineer.com/083-introducing-the-wordpress-wp-app-store/" title="Introducing the WordPress WP App Store">Your Website Engineer</a></td>
+			<td>Audio</td>
+		</tr>
+		<tr>
+			<td>Mar 20, 2012</td>
+			<td><a href="http://wpcandy.com/reports/wp-app-store-launches-in-april/" title="WP App Store aims to bring the theme and plugin shopping experience into the dashboard">WP Candy</a></td>
+			<td>Article</td>
+		</tr>
+		</table>
 
 		<p>This site is powered by a custom WordPress theme developed in Sublime 
 		Text and running on Linode.</p>
