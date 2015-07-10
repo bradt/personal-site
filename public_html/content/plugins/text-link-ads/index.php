599850851c6915d4f1731caaefb43c6557f4b1b1
Brad Touesnard <brad@touesnard.com>
2015-07-10 08:59:08 -0300

    Remove all plugins.

--- public_html/content/plugins/text-link-ads/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
-require '../../../wp-config.php';
-require TEMPLATEPATH.'/404.php';
--- public_html/content/plugins/wp-migrate-db-pro-cli/version.php
+++
@@ -1,3 +0,0 @@
-<?php
-$GLOBALS['wpmdb_meta']['wp-migrate-db-pro-cli']['version'] = '1.2';
-$GLOBALS['wpmdb_meta']['wp-migrate-db-pro-cli']['required-php-version'] = '5.3.2';
